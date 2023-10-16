54c5655296c245f06c035e026de66c5e43ee0c85
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-10-16 15:09:15 +0200

    regler card car

---
+++ .history/espace_admin/forgot_psw_20231016102227.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'connexion.php';
+require_once '../vendor/autoload.php';
+
+$transport = Swift_MailTransport::newInstance
+// if(isset($_POST['email'])){
+//     $password = uniqid();
+//     $hashPassword = password_hash($password,PASSWORD_DEFAULT);
+
+
+// //     // $to ='vinccentparrot@gmail.com';
+//     $subject ='testing sendmail.exe';
+//     $message ="Bonjour, voici votre nouveau mot de passe : $password";
+//    $headers = "Content-Type : text/plain; charset=utf-8\r\n";
+//     if(mail('vinccentparrot@localhost.com', 'Mot de passe oublié',$message,$headers)){
+//         $sql = "UPDATE admin SET password = ? WHERE email = ?";
+//         $stmt = $conn->prepare($sql);
+//         $stmt->execute([$hashPassword,$_POST['email']]);
+//         echo "Mail envoyé";
+//     }else{
+//         echo "erreur";
+//     }
+ //}
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+ <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>garage</title>
+        <meta name="description" content="Notre garage est parfaitement équipé, le Garage V.Parrot entretient vos véhicules et propose également des occasions toutes marques garanties. Il assure également le diagnostic et la réparation
+         toutes marques de vos véhicules.">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+          <!-- CSS Link -->
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+<div class="form-container">
+    <form action="" method="POST">
+        <h3>Recupération de mot de passe</h3>
+        <input type="email" name="email" required placeholder="entrer votre adresse mail">
+        <button type="submit" class="btn" name="recup_mdp" >Valider</button>
+        
+    </form>
+</div>
+
+<!-- if(mail($_POST['email'], 'Mot de passe oublié',$message, $headers)){ -->
---
+++ .history/templates/vehicules_20231016140354.php
@@ -0,0 +1,169 @@
+<?php
+require_once '../espace_admin/connexion.php';
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+ <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>garage</title>
+        <meta name="description" content="Notre garage est parfaitement équipé, le Garage V.Parrot entretient vos véhicules et propose également des occasions toutes marques garanties. Il assure également le diagnostic et la réparation
+         toutes marques de vos véhicules.">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <!-- <script src="jquery-3.6.4.min.js"></script>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js" integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
+     
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" rel="stylesheet"/>
+         <!--  carrousel -->
+         <link href="https://gosnippets.com/full-screen/bootstrap-carousel-with-cards-in-3-columns" rel="canonical">
+            <!-- Bootstrap icons CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css"> 
+        <!-- font awesome cdn link-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
+          
+            <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+            <!-- CSS Link -->
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+   
+<div class="wrapper js-filter">
+    <div class="row">
+        <div class="col-md-3 js-filter-form">
+    <div class="prix">
+    <h3>Prix</h3>
+    <p>Utiluser slider ou entrer min max prix</p>
+    <div class="field">
+        <span>Min</span>
+        <input type="number" class="input-min" value="1500">
+    </div>
+    <br>
+    <div class="field">
+        <span>Max</span>
+        <input type="number" class="input-max" value="150000">
+    </div>
+    <br>
+    <div class="slider">
+        <div class="progress"></div>
+    </div>
+    <div class="range-input">
+        <input type="range" class="range-min" min="0" max="10000" value="1500">
+        <input type="range" class="range-max" min="0" max="10000" value="150000">
+    </div>
+    <br>
+    </div>
+    <br>
+<div class="kilométrage">
+    <h3>kilométrage</h3>
+    <p>Utiluser slider ou entrer min max prix</p>
+    <div class="field">
+        <span>Min</span>
+        <input type="number" class="input-min" value="1500">
+    </div>
+    <br>
+    <div class="field">
+        <span>Max</span>
+        <input type="number" class="input-max" value="150000">
+    </div>
+    <br>
+    <div class="slider">
+        <div class="progress"></div>
+    </div>
+    <div class="range-input">
+        <input type="range" class="range-min" min="0" max="10000" value="1500">
+        <input type="range" class="range-max" min="0" max="10000" value="150000">
+    </div>
+    </div>
+    <br>
+  
+    <div class="Années">
+    <h3>Années</h3>
+    <p>Utiluser slider ou entrer min max prix</p>
+    <div class="field">
+        <span>Min</span>
+        <input type="number" class="input-min" value="1500">
+    </div>
+    <br>
+    <div class="field">
+        <span>Max</span>
+        <input type="number" class="input-max" value="150000">
+    </div>
+    <br>
+    <div class="slider">
+        <div class="progress"></div>
+    </div>
+    <div class="range-input">
+        <input type="range" class="range-min" min="0" max="10000" value="1500">
+        <input type="range" class="range-max" min="0" max="10000" value="150000">
+    </div>
+    </div>
+    <br>
+   </div>
+  
+   <div class="col-md-9 d-flex ">
+   <div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">BMW</h5>
+      <p class="card-text">Prix:</p>
+      <p class="card-text">Kilométrage:</p>
+    </div>
+    <button class="btn btn-primary">Détail</button>
+    <button type="submit" class="btn btn-primary">Contact</button>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+<div class="card-deck">
+  <div class="card">
+    <img class="card-img-top" src="..." alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  
+  </div>
+</div>
+   </div>
+   <div class="mt-4 js-filter-pagination">
+   <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav> 
+</div>
+</div>
+<script src="../js/script.js"></script>
+
+
+
+
+
+<?php
+require_once '../templates/footer.php';
+?>
