cdb3b53f7f8d9a59562b40ffdf1f19f3f241a6aa
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-10-18 20:15:34 +0200

    configuration systéme rating

---
+++ .history/js/script_20231018161837.js
@@ -0,0 +1,80 @@
+// -----------------reviews clients--------------
+$(document).ready(function(){
+  
+    var rating_data = 0;
+    $('#add_review').click(function(){
+        $('#review_modal').modal('show');
+    });
+    //changer la couleur de l'étoile en passant la souris
+    $(document).on('mousseenter','.')
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------filtre-----------------
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Écoutez le clic sur le bouton de filtrage
+//     document.getElementById('filter-button').addEventListener('click', function() {
+//         // Récupérez les valeurs des filtres (prix, kilométrage, année)
+//         var minPrice = document.querySelector('.prix .input-min').value;
+//         var maxPrice = document.querySelector('.prix .input-max').value;
+//         var minKilometrage = document.querySelector('.kilométrage .input-min').value;
+//         var maxKilometrage = document.querySelector('.kilométrage .input-max').value;
+//         var minAnnee = document.querySelector('.Années .input-min').value;
+//         var maxAnnee = document.querySelector('.Années .input-max').value;
+
+//         // Créez une instance XMLHttpRequest (XHR)
+//         var xhr = new XMLHttpRequest();
+
+//         // Configurez la requête XHR
+//         xhr.open('POST', 'votre_script_de_filtrage.php', true);
+//         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+//         // Gérez la réponse
+//         xhr.onload = function() {
+//             if (xhr.status === 200) {
+//                 // Traitez ici les données renvoyées par le backend
+//                 // Mettez à jour l'interface utilisateur avec les résultats
+//                 document.getElementById('results-container').innerHTML = xhr.responseText; // Vous devrez adapter ceci en fonction du format de vos résultats
+//             } else {
+//                 console.log('Erreur de la requête AJAX : ' + xhr.status);
+//             }
+//         };
+
+//         // Gérez les erreurs réseau
+//         xhr.onerror = function() {
+//             console.log('Erreur réseau lors de la requête AJAX');
+//         };
+
+//         // Envoyez les données de filtrage au backend
+//         var params = 'minPrice=' + minPrice + '&maxPrice=' + maxPrice + '&minKilometrage=' + minKilometrage + '&maxKilometrage=' + maxKilometrage + '&minAnnee=' + minAnnee + '&maxAnnee=' + maxAnnee;
+//         xhr.send(params);
+//     });
+// });
