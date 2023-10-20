8518d449c96b196dcb995e3844f5b973035c5bcf
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-10-20 20:35:16 +0200

    styliser footer

---
+++ .history/espace_admin/horaires_20231020170940.php
@@ -0,0 +1,26 @@
+<?php
+ require_once '../templates/config.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <!-- Bootstrap -->
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- CSS Link -->
+     <link rel="stylesheet" href="../css/style.css">
+</head>
+<section class>
+    <h2>Horaires d'ouverture / fermeture</h2>
+    <form >
+        <div class="form-group">
+            <select class="form-control" name="jour"></select>
+            <?php foreach(jours as $k=> $jours); ?>
+            <option value="<?= $k ?>"><?= $jours ?></option>
+            
+        </div>
+    </form>
+</section>
---
+++ .history/templates/footer_20231020175021.php
@@ -0,0 +1,50 @@
+<?php
+require_once 'head.php';
+require_once 'config.php';
+
+function creneaux_html (array $creneaux){
+  if(count($creneaux) === 0){
+    return 'Fermé';
+  }
+$phrases = [];
+foreach ($creneaux as $creneau){
+  $phrases[] = "de <strong> {$creneau[0]}h</strong> / <strong>{$creneau[1]}h</strong>";
+}
+return 'Ouvert '. implode(' - ', $phrases);
+};
+
+
+?>
+<footer class="d-flex flex-wrap justify-content-between align-items-center border-top">
+   <div class="row">
+        <div class="col-md-6 d-flex align-items-center">
+           <h3>CONTACTA</h3>
+          </div>
+          <ul class="nav col-md-4 justify-content-center d-flex">
+            <li class="ms-3"><a  href="#"><i class="fa-brands fa-github" style="color: #ffffff;"></i></a></li>
+            <li class="ms-3"><a  href="#"><i class="fa-brands fa-twitter" style="color: #fafdff;"></i></a></li>
+            <li class="ms-3"><a  href="#"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a></li>
+            <li class="ms-3"><a  href="#"><i class="fa-brands fa-facebook" style="color: #ffffff;"></i></a></li>
+          </ul>
+           <span class="mb-3 mb-md-0">© 2023 Company, Inc</span>
+      </div>
+    </div>
+    <div class="col-md-3 reviews_footer">
+      <h5>SATISFAIT DE NOS ?</h5>
+       <a  href="./templates/reviews.php" type="button" class="btn btn-primary">Ajouter un avis</a>
+    </div>
+   <div class="col-md-3 ">
+    <h3>les horaires d'ouverture</h3>
+    <ul>
+      <?php foreach(jours as $k=> $jours): ?>
+        <li>
+          <strong><?= $jours ?></strong> :
+          <?= creneaux_html(CRENEAUX[$k]); ?>
+      </li>
+      <?php endforeach ;?>
+    </ul>
+    
+   </div>
+   
+ </footer>
+
