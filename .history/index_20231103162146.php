485a3b1d5c4e4e154fa0ae6af652e5f67db3fee0
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-11-04 11:50:48 +0100

    créer détailles car

---
+++ .history/index_20231103162146.php
@@ -0,0 +1,59 @@
+<?php
+require_once './templates/services.php';
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+ <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>garage</title>
+        <meta name="description" content="Notre garage est parfaitement équipé, le Garage V.Parrot entretient vos véhicules et propose également des occasions toutes marques garanties. Il assure également le diagnostic et la réparation
+         toutes marques de vos véhicules.">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <!--  carrousel -->
+         <link
+         rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+            <!-- Bootstrap icons CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css"> 
+        <!-- font awesome cdn link-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
+          
+            <!-- Bootstrap -->
+            
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+            <!-- CSS Link -->
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+<!-- ------fetch reviews----- -->
+<?php
+require_once './templates/header.php';
+require_once ''
+?>
+<section>
+<h2 class="mt-5">Vos avis</h2>
+<?php
+require_once ('./espace_admin/connexion.php');
+     $req = $conn->query('SELECT * FROM review_table WHERE user_rating > 2');
+     while($user = $req->fetch()){
+        ?>
+<div class="container-reviews">  
+<div class="card text-center">
+  <div class="card-header"><?=$user['user_rating']?>
+    <i class="fas fa-star avis"></i>
+</div>
+  <div class="card-body">
+    <h5 class="ca-title"><?=$user['user_name']?></h5>
+    <p class="card-text"><?=$user['user_review']?></p>
+  </div>
+  <div class="card-footer text-body-secondary"><?=date('l jS, F Y h:i:s A', $user["datetime"])?></div>
+</div>
+</div> 
+
+        <?php 
+     }
+    ?>
+</section>
+    <?php
+require_once './templates/footer.php';
+?>
---
+++ .history/templates/détailles_car_20231103170628.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'head.php';
+?>
+<section>
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../uploads/pare_brise.jpg"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../uploads/mecanique.jpg"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../uploads/mercedes-benz-1370536_640.jpg"  alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
