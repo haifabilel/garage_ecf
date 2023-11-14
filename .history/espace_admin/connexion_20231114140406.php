2dc4cc11e336fdce8f9c76e59649da60b494525b
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-11-14 14:07:12 +0100

    database heroku

--- .history/espace_admin/connexion_20231114140406.php
+++ .history/espace_admin/connexion_20231114140406.php
@@ -1,11 +1,11 @@
 <?php
 
 //connexion à la bdd
-$user = 'w1pdb5f29zag14pu';
-$pass = 'xm22u8br12as85nr';
+$user = 'qbamrrogjwwnqm';
+$pass = 'eaf76d2d61ad45aa11fdc23d0a20835f86762ae3b87831e85ad2fc79c93dd00f';
 $host = 'q0h7yf5pynynaq54.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 try {
-    $conn = new PDO('mysql:host=$host;port=3306;dbname=gjkpoof9ijxk2nep',$user, $pass);
+    $conn = new PDO('mysql:host=$host;port=5432;dbname=ecf',$user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
  
 }catch(PDOException $e){
--- .history/espace_admin/connexion_20231114140450.php
+++ .history/espace_admin/connexion_20231114140450.php
@@ -5,7 +5,7 @@ $user = 'w1pdb5f29zag14pu';
 $pass = 'xm22u8br12as85nr';
 $host = 'q0h7yf5pynynaq54.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 try {
-    $conn = new PDO('mysql:host=$host;port=3306;dbname=gjkpoof9ijxk2nep',$user, $pass);
+    $conn = new PDO('mysql:host=$host;port=3306;dbname=ecf',$user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
  
 }catch(PDOException $e){
