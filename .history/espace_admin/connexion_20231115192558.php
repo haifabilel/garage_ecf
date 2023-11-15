262031a78b0e3234670b9bbb9eae67f48c17b358
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-11-15 23:26:39 +0100

    responsive mobile page details_car

---
+++ .history/espace_admin/connexion_20231115192558.php
@@ -0,0 +1,21 @@
+<?php
+
+//connexion à la bdd
+$user ='r';
+// $user ='dsf3j3ycnocrz0mt';
+// $pass ='kxtv9ef1towu7wlm';
+// $host ='esilxl0nthgloe1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
+// $db_name ='wyk5nombobh9yvg1';
+try {
+    // $conn = new PDO('mysql:$host;3306;dbname=$db_name',$user, $pass);
+    $conn = new PDO('mysql:host=localhost;dbname=ecf',$user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+ 
+}catch(PDOException $e){
+  // Enregistrement du message d'erreur dans le fichier bdlogs.log pour sécuriser les coordonnées.
+  file_put_contents('bdlogs.log', $e->getMessage().PHP_EOL, FILE_APPEND);
+
+}
+
+
+?>
