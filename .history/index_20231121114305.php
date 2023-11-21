7cf723f050fdd0eab0daa631368ec926ff4e95a2
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-11-21 11:44:15 +0100

    sémentique partie 2 templates

---
+++ .history/index_20231121114305.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+ <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>garage</title>
+        <meta name="description" content="Notre garage est parfaitement équipé, le Garage V.Parrot entretient vos véhicules et propose également des occasions toutes marques garanties. Il assure également le diagnostic et la réparation
+         toutes marques de vos véhicules.">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <!--  carrousel -->
+         <link
+         rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+            <!-- Bootstrap icons CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css"> 
+        <!-- font awesome cdn link-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer">
+            <!-- Bootstrap -->
+            
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+            <!-- CSS Link -->
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+<?php
+require_once './templates/header.php';
+require_once './templates/home.php';
+require_once './templates/services.php';
+require_once './espace_admin/fetch_avis.php';
+require_once './templates/footer_index.php';
+?>
---
+++ .history/templates/services_20231121114210.php
@@ -0,0 +1,25 @@
+<?php
+require_once './espace_admin/connexion.php';
+require_once 'head.php';
+?>
+
+<section class="service_section" id="services">
+    <h2>Nos Services</h2>
+  <div class="row">
+    <?php
+  $req = $conn->query('SELECT * FROM services');
+  while($user = $req->fetch()) {
+    ?>
+    <div class="col-lg-4 d-flex">
+      <div class="card">
+         <img class="card-img-top" src="./uploads/<?php echo $user['image']; ?>" alt="Card image cap">
+      <div class="card-body">
+          <h5 class="card-title_service text-center"><?php echo $user['titre']; ?></h5>
+          <p class="card-text"><?php echo $user['description']; ?></p>
+      </div>
+    </div>
+     
+      </div>
+      <?php } ;?>
+ 
+</section>
