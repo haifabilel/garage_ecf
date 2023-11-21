2131282c0c114b93dee6c092b10b5aa7d15b2067
haifabilel <125888354+haifabilel@users.noreply.github.com>
2023-11-21 14:03:20 +0100

    vérification fonctionalités

---
+++ .history/js/script_20231121120144.js
@@ -0,0 +1,111 @@
+// ----------------star rating-----------------
+
+// executer le button review
+var rating_data = 0;
+
+$('#add_review').click(function(){
+
+    $('#review_modal').modal('show');
+
+});
+//Changeer la couleur de l'etoile en passant le curseur
+$(document).on('mouseenter', '.submit_star', function(){
+
+    var rating = $(this).data('rating');
+
+    reset_background();
+
+    for(var count = 1; count <= rating; count++)
+    {
+
+        $('#submit_star_'+count).addClass('text-primary');
+
+    }
+
+});
+//Réinitialiser le background color de l'étoile
+function reset_background()
+{
+    for(var count = 1; count <= 5; count++)
+    {
+
+        $('#submit_star_'+count).addClass('star-light');
+
+        $('#submit_star_'+count).removeClass('text-primary');
+
+    }
+}
+
+$(document).on('mouseleave', '.submit_star', function(){
+
+    reset_background();
+
+    for(var count = 1; count <= rating_data; count++)
+    {
+
+        $('#submit_star_'+count).removeClass('star-light');
+
+        $('#submit_star_'+count).addClass('text-primary');
+    }
+
+});
+
+$(document).on('click', '.submit_star', function(){
+
+    rating_data = $(this).data('rating');
+
+});
+//enregitrer le choix de nombre d'étoile
+$('#save_review').click(function(){
+
+    var user_name = $('#user_name').val();
+
+    var user_review = $('#user_review').val();
+
+    if(user_name == '' || user_review == '')
+    {
+        alert("Veuillez remplir tous les champs");
+        return false;
+    }
+    else
+    {
+
+        $.ajax({
+            url:"reviews.php",
+            method:"POST",
+            data:{submit:submit, user_name:user_name, user_review:user_review},
+            success:function(data)
+            {
+                $('#review_modal').modal('hide');
+
+                load_rating_data();
+
+                alert(data);
+            }
+        })
+    }
+});
+// Affichage mot de passe dans le formulaire login
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById("password");
+    var eyeIcon = document.getElementById("eye-icon");
+  
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eyeIcon.className = "fa fa-eye-slash";
+    } else {
+      passwordInput.type = "password";
+      eyeIcon.className = "fa fa-eye";
+    }
+  };
+  
+  
+
+
+
+
+
+
+
+
+
---
+++ .history/templates/reviews_20231121115715.php
@@ -0,0 +1,75 @@
+<?php
+require_once '../espace_admin/connexion.php';
+require_once 'head.php';
+session_start();
+if(isset($_POST["rating_data"]))
+{
+
+	
+	$data = array(
+		':user_name'		=> htmlspecialchars($_POST['user_name'], ENT_QUOTES),
+		':user_rating'		=>	$_POST["rating_data"],
+		':user_review'		=>	htmlspecialchars($_POST['user_review'], ENT_QUOTES),
+		':datetime'			=>	time()
+	);
+
+	$query = "INSERT INTO review_table (user_name, user_rating, user_review, datetime) 
+	VALUES (:user_name, :user_rating, :user_review, :datetime)";
+
+	$statement = $conn->prepare($query);
+
+	$statement->execute($data);
+	/
+	$errors[] = "Merci pour votre témoignage";
+    
+};
+
+?>
+
+<div class="container_header p-3 ">
+    <div class="content_header d-flex">
+        <p>Donner votre avis</p>
+    </div>
+       <div class="content_button mt-2">
+          <a href="../index.php" class="btn btn-primary" id="Accueil_avis">Accueil  <i class="bi bi-backspace"></i></a>
+       </div>
+</div>
+<section>
+<?php
+        if(!empty($errors)){
+            foreach($errors as $error){
+                echo '<h3 class="error-msg" >'.$error.'</h3>';
+            };
+        };
+        ?>
+  	<div class="container_reviews">
+	  <h3>Votre avis nous interesse</h3>
+		<div class="card d-flex">
+	      	<h4 class="text-center stars mt-2 mb-4">
+				<i class="fas fa-star star-light submit_star mr-1" id="submit_star_1" data-rating="1"></i>
+				<i class="fas fa-star star-light submit_star mr-1" id="submit_star_2" data-rating="2"></i>
+				<i class="fas fa-star star-light submit_star mr-1" id="submit_star_3" data-rating="3"></i>
+				<i class="fas fa-star star-light submit_star mr-1" id="submit_star_4" data-rating="4"></i>
+				<i class="fas fa-star star-light submit_star mr-1" id="submit_star_5" data-rating="5"></i>
+	        </h4>
+            <form method="POST" enctype="multipart/form-data">
+	        	<div class="form-group">
+	        		<input type="text" name="user_name" id="user_name" class="form-control" placeholder="Entrer votre nom complét" />
+	        	</div><br>
+	        	<div class="form-group">
+	        		<textarea name="user_review" id="user_review" class="form-control " placeholder="Votre avis"></textarea>
+	        	</div>
+	        	<div class="form-group text-center mt-4">
+	        		<button type="submit" name="rating_data" class="btn btn-primary" id="save_review">Submit</button>
+	        	</div>
+            </form>
+		</div>
+    </div>
+</section>
+<script src="../js/script.js"></script>
+<?php require_once 'footer.php' ?>
+
+
+
+
+
